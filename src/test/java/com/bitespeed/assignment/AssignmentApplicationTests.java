876812a639b3659cd522f38fe707791ee9b10b01
sharath.majjigi <sharath.majjigi@vance.tech>
2024-06-06 02:19:38 +0530

    init project

---
+++ src/test/java/com/bitespeed/assignment/AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitespeed.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
